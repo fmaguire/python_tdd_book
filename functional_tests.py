270892d342c87008d1e4a2aeeb186ce0bdc73b63
Finlay Maguire <finlaymaguire@gmail.com>
2014-11-16 19:53:22 +0000

    First FT specced out in comments and refactored to unittest framework

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,41 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_user_can_start_a_list_and_retrieve_it_later(self):
+        # User goes to website homepage
+        self.browser.get('http://localhost:8000')
+
+        # User sees page has title and header about todo lists
+        self.assertIn('todo', self.browser.title)
+        self.fail('Finish the test!')
+
+        # User is invited to enter item to todo list
+
+        # User adds 'buy X' into a text box
+
+        # User hits enter and page updates and lists '1: buy X' as an item in todo list
+
+        # Text box persists and user adds 'sell Y' to it
+
+        # User presses enter and page updates and now shows both items
+
+        # User sees explanatory text that custom url has been generated for her list
+
+        # User checks custom url and confirms list is still there
+
+        # User leaves
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
