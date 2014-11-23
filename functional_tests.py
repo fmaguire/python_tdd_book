8ea83f2b38677f50349e9cdd97fbd23d4ab3a401
Finlay Maguire <finlaymaguire@gmail.com>
2014-11-23 19:43:01 +0000

    Refactor functional tests for table input
    Now uses function to check table contents and thus removes code
    duplication

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_user_can_start_a_list_and_retrieve_it_later(self):
         # User goes to website homepage
         self.browser.get('http://localhost:8000')
@@ -32,20 +37,15 @@ class NewVisitorTest(unittest.TestCase):
         # User hits enter and page updates and lists '1: buy X' as an item in todo list
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: buy X', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: buy X')
 
         # Text box persists and user adds 'sell Y' to it
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('sell Y')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: buy X', [row.text for row in rows])
-        self.assertIn('2: sell Y', [row.text for row in rows])
-
+        self.check_for_row_in_list_table('1: buy X')
+        self.check_for_row_in_list_table('2: sell Y')
 
 
         self.fail('Finish the test!')
