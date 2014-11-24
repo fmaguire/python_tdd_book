fe5fca7fbb602b97cd7401f0595e7864292de29d
Finlay Maguire <finlaymaguire@gmail.com>
2014-11-24 01:36:24 +0000

    New URL + view for adding to existing lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,6 @@
-from selenium import webdriver
 from django.test import LiveServerTestCase
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -12,12 +11,14 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_user_can_start_a_list_and_retrieve_it_later(self):
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # User1 goes to website homepage
         self.browser.get(self.live_server_url)
 
